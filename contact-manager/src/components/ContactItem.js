ae42765b802671806c84e41759cc6199df28586a
Harshita Nahata <khushinahata04@gmail.com>
2025-07-08 01:39:35 +0530

    fixing delete function error

--- contact-manager/src/components/ContactItem.js
+++ contact-manager/src/components/ContactItem.js
@@ -12,9 +12,10 @@ function ContactItem({ contact, onEdit, onDelete }) {
                     <IconButton edge="end" aria-label="edit" onClick={() => onEdit(contact)}>
                         <EditIcon />
                     </IconButton>
-                    <IconButton edge="end" aria-label="delete" onClick={() => onDelete(contact.email)}>
+                    <IconButton edge="end" aria-label="delete" onClick={() => onDelete(contact._id)}>
                         <DeleteIcon />
                     </IconButton>
+
                 </>
             }
         >
