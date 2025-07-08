dc2a34d69d334ad8e8e7974fee7f5ef26ddc8d52
Harshita Nahata <khushinahata04@gmail.com>
2025-07-08 18:57:09 +0530

    backend file fix for valid phone number

--- contact-manager-backend/models/Contact.js
+++ contact-manager-backend/models/Contact.js
@@ -5,7 +5,11 @@ const contactSchema = new mongoose.Schema({
     lastName: { type: String, required: true },
     address: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    phone: { type: String, required: true }
+    phone: {
+        type: String,
+        required: true,
+        match: [/^\d+$/, 'Phone number must contain only digits.']
+    }
 });
 
 module.exports = mongoose.model('Contact', contactSchema);
